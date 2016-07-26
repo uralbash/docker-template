22cea88a7fcaf226a662cdf37b361f247db31cc1
Svintsov Dmitry <root@uralbash.ru>
2016-07-26 11:42:02 +0500

    fix gitignore

---
+++ qt5/centos6-i386/example/build-app.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# build-app.sh
+# Copyright (C) 2016 uralbash <root@uralbash.ru>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+PROJECT_PATH=/home/vagrant/app
+
+# build Qt project
+cd ~/app \
+    && \
+    PATH=/opt/gcc-4.8.2/bin/:/opt/Qt-5.7/bin/:$PATH \
+    qmake \
+    && \
+    PATH=/opt/gcc-4.8.2/bin/:/opt/Qt-5.7/bin/:$PATH \
+    make
